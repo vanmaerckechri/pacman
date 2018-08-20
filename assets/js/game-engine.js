870f02ab71bbfd8e7743aace91e8746accbd6e3c
Christophe <vanmaerckechri@gmail.com>
2018-08-20 09:50:05 +0200

    add home

--- assets/js/game-engine.js
+++ assets/js/game-engine.js
@@ -44,8 +44,6 @@ function placeCanvasOnBackground()
 	canvasFood.style.top = canvasBackground.offsetTop + "px"
 }
 
-window.addEventListener("resize", placeCanvasOnBackground, false)
-
 // -- ADAPT SIZE TO SCREEN --
 function adaptGameSizeToScreen()
 {
@@ -79,75 +77,71 @@ function adaptToMobileLandscape()
     	document.getElementById("pacmanContainer").classList.add("pacmanLandscape");
     }
 }
-// -- LOAD MAP --
-
-let createHtml = function()
-{
-	let pacmanContainer = document.getElementById("pacmanContainer");
-
-	let ui = document.createElement("div");
-	ui.setAttribute("class", "ui");
-	let scoreContainer = document.createElement("div");
-	scoreContainer.setAttribute("class", "scoreContainer");
-	scoreContainer.innerText = "score: ";
-	let score = document.createElement("span");
-	score.setAttribute("id", "score");
-	score.innerText = "0";
-	let gameOverDiv = document.createElement("div");
-	gameOverDiv.setAttribute("id", "gameOver");
-	gameOverDiv.setAttribute("class", "gameOver");
-	gameOverDiv.innerText = "GameOver";
-	scoreContainer.appendChild(score);
-	ui.appendChild(scoreContainer);
-	ui.appendChild(gameOverDiv);
-	pacmanContainer.appendChild(ui);
-
-	let canvasContainer = document.createElement("div");
-	canvasContainer.setAttribute("id", "canvasContainer");
-	canvasContainer.setAttribute("class", "canvasContainer");
-	let canvasBackground = document.createElement("canvas");
-	canvasBackground.setAttribute("id", "canvasBackground");
-	canvasBackground.setAttribute("class", "canvasBackground");
-	let canvasFood = document.createElement("canvas");
-	canvasFood.setAttribute("id", "canvasFood");
-	canvasFood.setAttribute("class", "canvasFood");
-	let canvasPlayer = document.createElement("canvas");
-	canvasPlayer.setAttribute("id", "canvasPlayer");
-	canvasPlayer.setAttribute("class", "canvasPlayer");
-	let canvasGhosts = document.createElement("canvas");
-	canvasGhosts.setAttribute("id", "canvasGhosts");
-	canvasGhosts.setAttribute("class", "canvasGhosts");
-	canvasContainer.appendChild(canvasBackground);
-	canvasContainer.appendChild(canvasFood);
-	canvasContainer.appendChild(canvasPlayer);
-	canvasContainer.appendChild(canvasGhosts);
-	pacmanContainer.appendChild(canvasContainer);
-
-	let mobilePad = document.createElement("div");
-	mobilePad.setAttribute("id", "mobilePad");
-	mobilePad.setAttribute("class", "mobilePad");
-	let mobilePadTop = document.createElement("div");
-	mobilePadTop.setAttribute("class", "mobilePadTop");
-	let mobilePadRight = document.createElement("div");
-	mobilePadRight.setAttribute("class", "mobilePadRight");
-	let mobilePadBottom = document.createElement("div");
-	mobilePadBottom.setAttribute("class", "mobilePadBottom");
-	let mobilePadLeft = document.createElement("div");
-	mobilePadLeft.setAttribute("class", "mobilePadLeft");
-
-	mobilePad.appendChild(mobilePadTop);
-	mobilePad.appendChild(mobilePadRight);
-	mobilePad.appendChild(mobilePadBottom);
-	mobilePad.appendChild(mobilePadLeft);
-	pacmanContainer.appendChild(mobilePad);
-}
+// -- LAUNCH / STOP GAME --
 
 let pacmanGameLaunched = false
 let launchPacmanGame = function()
 {
 	if (pacmanGameLaunched == false)
 	{
-		createHtml();
+		//create html content
+		let pacmanContainer = document.getElementById("pacmanContainer");
+		let ui = document.createElement("div");
+		ui.setAttribute("class", "ui");
+		let scoreContainer = document.createElement("div");
+		scoreContainer.setAttribute("class", "scoreContainer");
+		scoreContainer.innerText = "score: ";
+		let score = document.createElement("span");
+		score.setAttribute("id", "score");
+		score.innerText = "0";
+		let gameOverDiv = document.createElement("div");
+		gameOverDiv.setAttribute("id", "gameOver");
+		gameOverDiv.setAttribute("class", "gameOver");
+		gameOverDiv.innerText = "GameOver";
+		scoreContainer.appendChild(score);
+		ui.appendChild(scoreContainer);
+		ui.appendChild(gameOverDiv);
+		pacmanContainer.appendChild(ui);
+
+		let canvasContainer = document.createElement("div");
+		canvasContainer.setAttribute("id", "canvasContainer");
+		canvasContainer.setAttribute("class", "canvasContainer");
+		let canvasBackground = document.createElement("canvas");
+		canvasBackground.setAttribute("id", "canvasBackground");
+		canvasBackground.setAttribute("class", "canvasBackground");
+		let canvasFood = document.createElement("canvas");
+		canvasFood.setAttribute("id", "canvasFood");
+		canvasFood.setAttribute("class", "canvasFood");
+		let canvasPlayer = document.createElement("canvas");
+		canvasPlayer.setAttribute("id", "canvasPlayer");
+		canvasPlayer.setAttribute("class", "canvasPlayer");
+		let canvasGhosts = document.createElement("canvas");
+		canvasGhosts.setAttribute("id", "canvasGhosts");
+		canvasGhosts.setAttribute("class", "canvasGhosts");
+		canvasContainer.appendChild(canvasBackground);
+		canvasContainer.appendChild(canvasFood);
+		canvasContainer.appendChild(canvasPlayer);
+		canvasContainer.appendChild(canvasGhosts);
+		pacmanContainer.appendChild(canvasContainer);
+
+		let mobilePad = document.createElement("div");
+		mobilePad.setAttribute("id", "mobilePad");
+		mobilePad.setAttribute("class", "mobilePad");
+		let mobilePadTop = document.createElement("div");
+		mobilePadTop.setAttribute("class", "mobilePadTop");
+		let mobilePadRight = document.createElement("div");
+		mobilePadRight.setAttribute("class", "mobilePadRight");
+		let mobilePadBottom = document.createElement("div");
+		mobilePadBottom.setAttribute("class", "mobilePadBottom");
+		let mobilePadLeft = document.createElement("div");
+		mobilePadLeft.setAttribute("class", "mobilePadLeft");
+
+		mobilePad.appendChild(mobilePadTop);
+		mobilePad.appendChild(mobilePadRight);
+		mobilePad.appendChild(mobilePadBottom);
+		mobilePad.appendChild(mobilePadLeft);
+		pacmanContainer.appendChild(mobilePad);
+		// init and launche game loop
 		initCanvas();
 		initMap();
 		adaptGameSizeToScreen();
@@ -164,11 +158,42 @@ let launchPacmanGame = function()
 		placeCanvasOnBackground();
 		window.requestAnimationFrame(engine); 
 		//pacmanGameLaunched = true;
+		window.addEventListener("resize", placeCanvasOnBackground, false);
+	}
+}
+
+let launchPacmanHome = function()
+{
+	if (pacmanGameLaunched == false)
+	{
+		let pacmanContainer = document.getElementById("pacmanContainer");
+		let tuto = document.createElement("div");
+		tuto.setAttribute("class", "pacmanTuto");
+		let tutoTitle = document.createElement("div");
+		tutoTitle.setAttribute("class", "tutoTitle");
+		tutoTitle.innerText = "Partie Bonus";
+		let tutoContent = document.createElement("div");
+		tutoContent.setAttribute("class", "tutoContent");
+		tutoContent.innerText = "blablabla les règles...";
+		let launchGameButton = document.createElement("button");
+		launchGameButton.setAttribute("class", "pacmanButton launchGameButton");
+		launchGameButton.setAttribute("id", "launchGameButton");
+		launchGameButton.innerText = "commencer"
+		tuto.appendChild(tutoTitle);
+		tuto.appendChild(tutoContent);
+		tuto.appendChild(launchGameButton);
+		pacmanContainer.appendChild(tuto);
+		launchGameButton.onclick = function()
+		{
+			tuto.remove();
+			launchPacmanGame();
+		}
 	}
 }
 
 function closeGame()
 {
+	window.removeEventListener("resize", placeCanvasOnBackground, false);
 	let cancelAnimationFrame = window.cancelAnimationFrame || window.mozCancelAnimationFrame;
 	cancelAnimationFrame(canvasAnimation);
 
@@ -180,6 +205,7 @@ function closeGame()
         	clearInterval(mapBoards[r][c].foodTime);
         }
     }
+
     clearTimeout(bonusTempo);
 	canvasAnimation = null;
 	tileSize = null;
@@ -221,5 +247,5 @@ function closeGame()
 
 window.addEventListener("load", function()
 {
-	launchPacmanGame();
+	launchPacmanHome();
 });
